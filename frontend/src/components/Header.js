312f2b83fe4cdf853bf5cda1e77759b0e17dc48f
eNgeLzz <142221443+eNgeLzzRus@users.noreply.github.com>
2025-05-12 09:34:24 +0700

    Update Header.js

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,26 +1,23 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import '../styles/Header.css';  // Подключение файла стилей
+import '../styles/Header.css'; 
 
 const Header = () => {
     const navigate = useNavigate();
 
-    // Функция для выхода из чата (покидает чат и перенаправляет на страницу входа)
     const handleLogout = () => {
-        localStorage.removeItem('token');  // Удаление токена из localStorage
-        localStorage.removeItem('username');  // Удаление имени пользователя из localStorage
-        navigate('/login');  // Перенаправление на страницу авторизации
+        localStorage.removeItem('token');  
+        localStorage.removeItem('username'); 
+        navigate('/login');  
     };
 
     return (
         <div className="header-container">
-            {/* Логотип и название проекта */}
             <div className="header-logo">
                 <img src='./logo.png' />
                 <h1>GALARD CHAT</h1>
             </div>
             
-            {/* Кнопка выхода из чата */}
             <button className="header-button" onClick={handleLogout}>
                 Покинуть чат
             </button>
